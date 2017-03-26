d62424e77cf4d03a70c2e1d74613de56b5904193
Rick Henry <fredericmhenry at gmail dot com>
2017-03-26 03:57:38 -0400

    Some notational updates
    I was planning to use underscores for files and changing case for
    variables but it turned out to be way too annoying so I changed it to
    all underscores. Much easier and works just as well.

--- get_instruments.rb
+++ get_instruments.rb
@@ -3,8 +3,9 @@
 # project.
 
 def get_instruments()
-  puts 'Please enter EACH instrument in the project separated by commas: (e.g. violin 1, violin 2, viola, etc.)'
-  instruments = gets.chomp.split(', ').map { |ins| ins.to_s.tr(' A-Z', '_a-z') }
+  puts 'Please enter EACH instrument in the project separated by commas: '
+  puts '(e.g. violin 1, violin 2, viola, etc.)'
+  parts = gets.chomp.split(', ').map { |ins| ins.to_s.tr(' A-Z', '_a-z') }
   # normalize instruments list to instrument_name_[number]
   # supports up to ins_9, but have to be entered individually.
   # if you want more, enter them properly (preferably with leading zeros on
@@ -13,7 +14,7 @@ def get_instruments()
   # efficient way to handle this but I can't seem to figure it out. sigh.
   # It does need to be VERY specific to avoid replacing random letters or
   # truncating whole words from instrument names. 
-  instruments.each do |ins| 
+  parts.each do |ins| 
     # this replaces roman numberals
     ins.gsub!(/_[ivx]*$/,  
               '_i'    => '_1',
@@ -37,8 +38,21 @@ def get_instruments()
     ins.gsub!(/_eight$/, '_8')
     ins.gsub!(/_nine$/, '_9')
   end
-  instruments
+  instrument_vars = parts.map { |ins| ins.gsub(/_[0-9]*$/,
+                                               '_1' => '_one',
+                                               '_2' => '_two',
+                                               '_3' => '_three',
+                                               '_4' => '_four',
+                                               '_5' => '_five',
+                                               '_6' => '_six',
+                                               '_7' => '_seven',
+                                               '_8' => '_eight',
+                                               '_9' => '_nine'
+                                              ) }
+  return parts, instrument_vars
 end
 
 #things = get_instruments()
-#things.each { |x| print x + "\n" }
+#print things.first
+#print things.last
+
--- get_movements.rb
+++ get_movements.rb
@@ -3,13 +3,13 @@
 # movement numbers for use in note variables. Supports up to twelve movements.
 
 def get_movements()
-  ordinal_movements = ['FirstMov', 'SecondMov', 'ThirdMov', 'FourthMov', 
-                       'FifthMov', 'SixthMov', 'SeventhMov', 'EighthMov',
-                       'NinthMov', 'TenthMov', 'EleventhMov', 'TwelfthMov']
+  ordinal_movements = ['first_mov', 'second_mov', 'third_mov', 'fourth_mov', 
+                       'fifth_mov', 'sixth_mov', 'seventh_mov', 'eighth_mov',
+                       'ninth_mov', 'tenth_mov', 'eleventh_mov', 'twelfth_mov']
   print 'Please enter the number of movements for the project: '
   movements = gets.chomp.to_i
   ordinal_movements.take(movements)
 end
 
-things = get_movements()
-things.each { |x| print x + "\n" }
+#things = get_movements()
+#things.each { |x| print x + "\n" }
