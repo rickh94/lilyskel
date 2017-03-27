ecaeeb893e614aba25614b5aba32814d594991d9
Rick Henry <fredericmhenry at gmail dot com>
2017-03-27 01:24:24 -0400

    Wrote Output and Score Classes
    Output class will create a file, write the top of it, write the bottom of it,
    and close it.
    
    Score changes the filename to be what is needed for a score and the name for
    the 'instrument' header field.

--- methods/generate_filenames.rb
+++ methods/generate_filenames.rb
@@ -4,20 +4,78 @@
 
 # Creating new classes
 class Output
+  
   def initialize(version, language, headers, instruments)
-  def filename(headers)
-    # Use opus or title for prefix. Prefer opus.
-    headers.has_key?("opus") ? 
-      prefix = headers['opus'].gsub('Op. ', 'O').tr(' ', '_').upcase :
-      prefix = headers['title'].downcase.tr(' ', '_').gsub('.', '')
-
-    #  Make the score filename
-    score_name = Array.new
-    score_name << prefix
-    score_name << 'score.ly'
-    score_name.join('_').to_s
+    @vers = version
+    @lang = language
+    @heads = headers
+    @insts = instruments
   end
 
+  def name()
+    'superceded'
+  end
+
+  # This will be superceded by subclasses
+  def filename()
+    'not_sublcass'
+  end
+
+  # Create the file.
+  def create()
+    @file = File.new(filename(), "w")
+  end
+
+  # Write the top of the file
+  def write_top()
+    @file.puts @vers
+    @file.puts @lang
+    @file.puts "\n#(ly:set-option 'relative-includes #t)"
+    @file.puts '\include "defs.ily"'
+    @file.puts '\header {'
+    @file.puts '  instrument = "' + name() + '"'
+    @file.puts "}\n\n"
+    @file.puts '\book {'
+  end
+
+  # Write the final closing bracket, possibly page info later
+  def write_bottom()
+    @file.puts '}'
+  end
+
+  # close the file
+  def done()
+    @file.close
+  end
+
+end
+
+class Score < Output
+  # Filename generation for Score.
+  def filename()
+    # Use opus or title for prefix. Prefer opus.
+    @heads.has_key?("opus") ? 
+      prefix = @heads['opus'].gsub('Op. ', 'O').tr(' ', '_').upcase :
+      prefix = @heads['title'].downcase.tr(' ', '_').gsub('.', '')
+
+    #  Make the score filename
+    new_name = Array.new
+    new_name << prefix
+    new_name << 'score.ly'
+    new_name.join('_').to_s
+  end
+
+  def name()
+    "Score"
+  end
+end
+
+tests = { "opus" => "Op. 15", "title" => "Test Title"}
+score = Score.new("vers", "lang", tests, "instruments")
+score.create()
+score.write_top()
+score.write_bottom()
+score.done()
 
 def part_filename(headers, instrument)
   filename = String.new
@@ -43,6 +101,3 @@ def part_filename(headers, instrument)
   filenames
 end
 
-#head = get_headers()
-#ins = get_instruments()
-#puts generate_filenames(head, ins)
