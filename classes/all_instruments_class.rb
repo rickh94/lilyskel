d18bd2cfe6456be3ebc5c90a1cf2ba0a866bd917
Rick Henry <fredericmhenry at gmail dot com>
2017-03-27 23:25:05 -0400

    Created AllInstruments class
    AllInstruments class in all_instruments_class.rb that gets and normalizes
    instrument names and creates Instrument objects for use later. Some methods
    split up. Code is much cleaner now.

---
+++ classes/all_instruments_class.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby 
+#  all_instruments_class.rb - a class for getting instruments from 
+#  user, normalizing their names and making an Instrument object for each.
+
+#require './instrument_class.rb'
+
+class AllInstruments
+  @@input = Array.new
+  @@all = Array.new
+
+  def get_instruments()
+    # Get instruments from user
+    puts 'Please enter each instrument in the project separated by commas: '
+    puts '(e.g. violin 1, violin 2, viola, etc.)'
+    @@input = gets.chomp.split(',').map{|i| i.to_s.gsub(/^ /, '')}
+  end
+
+  def remove_spaces_and_caps()
+    @@input.map{ |i| i.tr!(' A-Z', '_a-z') }
+  end
+
+  def normalize_roman_numerals()
+    @@input.map{ |i| i.gsub!(/_[ivx]*$/,
+                            '_i'    => '_1',
+                            '_ii'   => '_2',
+                            '_iii'  => '_3',
+                            '_iv'   => '_4',
+                            '_v'    => '_5',
+                            '_vi'   => '_6',
+                            '_vii'  => '_7',
+                            '_viii' => '_8',
+                            '_ix'   => '_9',
+                           ) }
+  end
+
+  def normalize_number_words()
+    @@input.each do |i|
+      i.gsub!(/_one$/, '_1')
+      i.gsub!(/_two$/, '_2')
+      i.gsub!(/_three$/, '_3')
+      i.gsub!(/_four$/, '_4')
+      i.gsub!(/_five$/, '_5')
+      i.gsub!(/_six$/, '_6')
+      i.gsub!(/_seven$/, '_7')
+      i.gsub!(/_eight$/, '_8')
+      i.gsub!(/_nine$/, '_9')
+    end
+  end
+
+  def create_instrument_objects()
+    @@input.each do |i|
+      tmp = i
+      @@all << Instrument.new(tmp.to_s)
+    end
+  end
+
+
+  def initialize()
+    get_instruments()
+    remove_spaces_and_caps()
+    normalize_roman_numerals()
+    normalize_number_words()
+    create_instrument_objects()
+  end
+
+  def all()
+    @@all
+  end
+end
+
+#puts AllInstruments.new.all()
+# TESTS
+#test = All_Instruments.new
+#puts Instrument.all_instances
+#puts test.all()
+#test.all().each do |i|
+#  puts i.pretty()
+#end
--- classes/info_class.rb
+++ classes/info_class.rb
@@ -3,63 +3,26 @@
 # lilypond project (except includes which are handled elsewhere).
 
 # Require statements for testing purposes only
-require './instrument_class.rb'
-require './headers_class.rb'
-require './version_class.rb'
+#require './instrument_class.rb'
+#require './headers_class.rb'
+#require './version_class.rb'
+#require './language_class.rb'
+#require './version_class.rb'
+#require './all_instruments_class.rb'
 
 class Info
   attr_reader :instruments, :headers, :version, :language
-  def get_instruments()
-    # Get instruments from user
-    puts 'Please enter each instrument in the project separated by commas: '
-    puts '(e.g. violin 1, violin 2, viola, etc.)'
-    parts = gets.chomp.split(',').map { |ins| ins.to_s.gsub(/^ /, '').tr(' A-Z', '_a-z') }
-
-    # normalize instruments list to instrument_name_[number]
-    # supports up to ins_9, but have to be entered individually.
-    # if you want more, you have to edit the class. 
-    parts.each do |ins| 
-      # this replaces roman numberals
-      ins.gsub!(/_[ivx]*$/,  
-                '_i'    => '_1',
-                '_ii'   => '_2',
-                '_iii'  => '_3',
-                '_iv'   => '_4',
-                '_v'    => '_5',
-                '_vi'   => '_6',
-                '_vii'  => '_7',
-                '_viii' => '_8',
-                '_ix'   => '_9',
-               )
-      # this replaces number words with numbers
-      ins.gsub!(/_one$/, '_1')
-      ins.gsub!(/_two$/, '_2')
-      ins.gsub!(/_three$/, '_3')
-      ins.gsub!(/_four$/, '_4')
-      ins.gsub!(/_five$/, '_5')
-      ins.gsub!(/_six$/, '_6')
-      ins.gsub!(/_seven$/, '_7')
-      ins.gsub!(/_eight$/, '_8')
-      ins.gsub!(/_nine$/, '_9')
-      file_name = ins
-      # Create new Instrument object for each instrument.
-      file_name = Instrument.new(file_name.to_s)
-    end
-    #  Return array of all instruments (tracked internally by Instrument class.
-    return Instrument.all_instances
-  end
-
   def initialize()
     @version = Version.new.self
     @language = Language.new.self
-    @instruments = get_instruments()
+    @instruments = AllInstruments.new.all()
     @headers = Headers.new
   end
 end
 
 # TESTS for info
-info = Info.new
-puts info.version
-puts info.language
-puts info.headers.all()
-info.instruments.each{ |i| puts i.pretty() }
+#info = Info.new
+#puts info.version
+#puts info.language
+#puts info.headers.all()
+#info.instruments.each{ |i| puts i.pretty() }
